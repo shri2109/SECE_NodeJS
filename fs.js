65f7f9267ae9ec1f11999c137bfe4b3655151d81
shri2109 <20euec143@skcet.ac.in>
2024-03-09 12:34:20 +0530

    read and write file asynchronously

--- fs.js
+++ fs.js
@@ -10,9 +10,19 @@ const fs = require('fs')
  * write : writeFileSync, writeFile
  */
 
-const file = fs.readFileSync('./data.json')
-const data = JSON.parse(file)
-console.log(data)
-data.name = 'abc'
+// Synchronous approach
+// const file = fs.readFileSync('./data.json')
+// const data = JSON.parse(file)
+// console.log(data)
+// data.name = 'abc'
+// fs.writeFileSync('./data.json', JSON.stringify(data))
 
-fs.writeFileSync('./data.json', JSON.stringify(data))
+// Asynchronous approach
+fs.readFile('./data.json', function(error, file) {
+    const data = JSON.parse(file)
+    console.log(data)
+    data.name = 'xyz'
+    fs.writeFile('./data.json', JSON.stringify(data), function() {
+        console.log('file written successfully...')
+    })
+})
