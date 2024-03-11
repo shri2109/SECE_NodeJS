6f06f57e70131dc1d6b0921e247273cbf56351bb
shri2109 <20euec143@skcet.ac.in>
2024-03-11 12:38:49 +0530

    post request

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+const bodyParser = require('body-parser')
 const express = require('express')
 
 const app = express()
+app.use(bodyParser.json())
 
 /**
  * 200 - ok
  * 201 - created
+ * 401 - unauthorized
  * 404 - page not found
  * 500 - internal server error
  */
@@ -24,6 +27,25 @@ app.get('/java', function(request, response) {
         "about" : "java",
         "description" : "Java is an object oriented programming language"
     })
+})
+
+/**
+ * request body
+ * params
+ * queries
+ */
+app.post('/validate-user', function(request, response) {
+    if(request.body.username === 'abcd' && request.body.password === '1234') {
+        response.status(200).json({
+            "status" : "success",
+            "message" : "valid user"
+        })
+    } else {
+        response.status(401).json({
+            "status" : "failure",
+            "message" : "invalid user"
+        })
+    }
 })
 
 app.listen(8000, function() {
