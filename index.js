b7c8779158ca649ab48041bf60620072c23988fe
shri2109 <20euec143@skcet.ac.in>
2024-03-11 10:35:00 +0530

    send json response

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const express = require('express')
 
 const app = express()
 
+/**
+ * 200 - ok
+ * 201 - created
+ * 404 - page not found
+ * 500 - internal server error
+ */
+
 app.get('/', function(request, response) {
-    response.send('hello')
+    // console.log(request)
+    // response.status(200).send('hello')
+    const obj = {
+        "greetings" : "welcome"
+    }
+    response.status(200).json(obj)
 })
 
 app.get('/java', function(request, response) {
-    response.send('Java is an object oriented programming language')
+    // response.status(200).send('Java is an object oriented programming language')
+    response.status(200).json({
+        "about" : "java",
+        "description" : "Java is an object oriented programming language"
+    })
 })
 
 app.listen(8000, function() {
